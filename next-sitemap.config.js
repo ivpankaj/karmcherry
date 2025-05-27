b6e246d2709d102a93e96127b0cd6faef5a33f13
Pankaj <imvpankaj@gmail.com>
2025-05-27 17:06:11 +0530

    as

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://karmcherry.web.app', // ✅ Your live site URL
+  generateRobotsTxt: true, // ✅ Also generate robots.txt
+  sitemapSize: 7000, // Optional: split sitemaps if needed
+};
